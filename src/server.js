93e019b8c7418251d4d2c730bbe43c2f2f51e1f0
Nicholas Reid <nicholasreid845@gmail.com>
2021-03-10 11:25:16 -0500

    're-commit'

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const knex = require('knex')
 const app = require('./app')
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 const { PORT, DATABASE_URL } = require('./config')
 
